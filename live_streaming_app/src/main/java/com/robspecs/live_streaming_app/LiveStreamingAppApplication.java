255f7021715be749f3d90fada2d04ce01339e022
Ankit Pandey <ankitpandey991@gmail.com>
2025-06-13 14:37:47 +0530

    BASIC SETUP DONE

---
+++ live_streaming_app/src/main/java/com/robspecs/live_streaming_app/LiveStreamingAppApplication.java
@@ -0,0 +1,13 @@
+package com.robspecs.live_streaming_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveStreamingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveStreamingAppApplication.class, args);
+	}
+
+}
